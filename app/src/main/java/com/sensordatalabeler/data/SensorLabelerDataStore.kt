a97e726018958b2ddee984c88c03e81eb2ff769c
siruma <65708321+siruma@users.noreply.github.com>
2024-11-28 22:18:30 +0200

    Fixed test and errors in code (#32)
    * Fixed test and errors in code
    
    Fixed wrong name in the button
    Added missing icon
    
    * Fixed all Warnings

--- app/src/main/java/com/sensordatalabeler/data/SensorLabelerDataStore.kt
+++ app/src/main/java/com/sensordatalabeler/data/SensorLabelerDataStore.kt
@@ -2,7 +2,13 @@ package com.sensordatalabeler.data
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.doublePreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.sensordatalabeler.data.db.MyLocationEntity
 import kotlinx.coroutines.flow.Flow
